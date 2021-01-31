d087b5c87f92c392ca0daa2fbb3cc82273f9303c
Jason R. Coombs <jaraco@jaraco.com>
2021-01-31 17:45:43 -0500

    ⚫ Fade to black.

--- jaraco/docker.py
+++ jaraco/docker.py
@@ -8,17 +8,14 @@ from jaraco.context import suppress
 @functools.partial(compose, bool)
 @suppress(FileNotFoundError)
 def text_in_file(text, filename):
-	return any(text in line for line in open(filename))
+    return any(text in line for line in open(filename))
 
 
 def is_docker():
-	"""
-	Is this current environment running in docker?
+    """
+    Is this current environment running in docker?
 
-	>>> type(is_docker())
-	<class 'bool'>
-	"""
-	return (
-		os.path.exists('/.dockerenv')
-		or text_in_file('docker', '/proc/self/cgroup')
-	)
+    >>> type(is_docker())
+    <class 'bool'>
+    """
+    return os.path.exists('/.dockerenv') or text_in_file('docker', '/proc/self/cgroup')
